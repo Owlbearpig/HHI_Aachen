6fa031c7d2278356f3df1fafac4abd4e4ee4d64f
Owlbearpig <alexj07@gmail.com>
2023-12-13 20:33:38 +0100

    - Test of teval package

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from teval.Measurements.image import Image
+import matplotlib.pyplot as plt
+from teval import Image
 from pathlib import Path
+import numpy as np
 
-path_ = Path("/home/ftpuser/ftp/Data/HHI_Aachen/sample3/img1")
+sample_idx = 4
+
+# path_ = Path("/home/ftpuser/ftp/Data/HHI_Aachen/sample3/img1")
+
+path_ = Path(f"E:\measurementdata\HHI_Aachen\sample{sample_idx}\img1")
 
 
 def main():
-    img = Image(path_)
+    if sample_idx == 3:
+        options = {"cbar_min": 1, "cbar_max": 3.0}
+    else:
+        options = {"cbar_min": 1, "cbar_max": 3.0}
+    img = Image(path_, options=options, sample_idx=sample_idx)
     img.plot_image()
+
+    plt.show()
 
 
 if __name__ == '__main__':
